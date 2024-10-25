eb8c6ae4f5d849fdfd466af0a13460e15c41b4eb
Umang Za;a <umangzala2k@gmail.com>
2024-10-25 17:40:33 +0530

    fix: router redirection

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cluster/performance-metrics',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+
